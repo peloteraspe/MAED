f5992ec6f97fdd509fa679e63e4d2ebc5675130c
andrealemonroy <andreamonroy@MacBook-Air-de-Andrea.local>
2025-03-09 16:15:37 -0500

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
 
 export default async function Home() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
+  const supabase = createClient();
 
   const { data: eventsData } = await supabase
     .from('event')
